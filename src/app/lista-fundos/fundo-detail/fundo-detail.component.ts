fbe1f303e9f037ddf0621ab86154251a3eebb9d0
Raphael Americano <raphael@raphaelamericano.com.br>
2020-10-06 16:34:05 -0300

    Animacao de collapse

--- src/app/lista-fundos/fundo-detail/fundo-detail.component.ts
+++ src/app/lista-fundos/fundo-detail/fundo-detail.component.ts
@@ -1,22 +1,49 @@
 import { Fundo } from './../../core/models/fundo.model';
 import { AfterViewInit, Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { state, style, transition, trigger, animate } from '@angular/animations';
+import { state, style, transition, trigger, animate, keyframes } from '@angular/animations';
 @Component({
   selector: 'app-fundo-detail',
   templateUrl: './fundo-detail.component.html',
   styleUrls: ['./fundo-detail.component.scss'],
   animations: [
-    trigger('state', [
-      state('void, hidden', style({ transform: 'scale(0)'})),
-      state('visible', style({ transform: 'scale(1)'})),
-      transition('* => visible', animate('500ms ease-in-out')),
-      transition('* => hidden', animate('500ms ease-in-out'))
-    ])
+    trigger('tr', [
+      state('void', style({ height: '0px', minHeight: 0, display: 'block'})),
+      state('*', style({ height: '*', display: 'table-row' })),
+      transition(':enter', animate(250, keyframes([
+        style({ offset: 0, height: '0px'}),
+        style({ offset: 0.25, height: '150px'}),
+        style({ offset: 0.5, height: '200px'}),
+        style({ offset: 0.75, height: '250px'}),
+        style({ offset: 0.80, height: '300px'}),
+        style({ offset: 1, height: '300px'})
+      ]))),
+      transition(':leave', animate(250, keyframes([
+        style({ offset: 0, height: '300px', display: 'block'}),
+        style({ offset: 0.25, height: '250px'}),
+        style({ offset: 0.5, height: '200px'}),
+        style({ offset: 0.75, height: '150px'}),
+        style({ offset: 1, height: '0px'})
+      ]))),
+      ]),
+    trigger('td', [
+      state('void', style({ opacity: 0, visibility: 'hidden'})),
+      state('*', style({ opacity: 1, visibility: 'visible' })),
+      transition(':enter', animate(1000))
+      // transition(':enter', animate(1000, keyframes([
+      //   style({ offset: 0}),
+      //   style({ offset: 0.25}),
+      //   style({ offset: 0.5}),
+      //   style({ offset: 0.75}),
+      //   style({ offset: 0.85, opacity: 0.5}),
+      //   style({ offset: 0.95, opacity: 0.75}),
+      //   style({ offset: 1, opacity: 1})
+      // ])))
+    ] )
   ]
 })
 export class FundoDetailComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  visibility: string = 'visible';
+  openClose: string = 'close';
 
   @Input()
   data: Fundo;
@@ -27,7 +54,7 @@ export class FundoDetailComponent implements OnInit, AfterViewInit, OnDestroy {
   constructor() { }
 
   ngOnInit(): void {
-    
+    this.openClose = 'open';
   }
 
   ngAfterViewInit(): void {
@@ -35,7 +62,7 @@ export class FundoDetailComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-   this.visibility = 'hidden' 
+    this.openClose = 'close';
   }
 
 }
